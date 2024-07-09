1b1bd0dbd0d00290d18674675d557c0e3d312ba3
Abdullah-encrypts <Dev.AbdullahAwan@gmail.com>
2024-07-09 07:05:55 +0500

    Basic Interface built

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,33 @@
 console.log("Client side js running!");
+
+const weatherForm = document.querySelector("form");
+const formInput = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+
+// messageOne.textContent = 'From javascript.....'
+
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const location = formInput.value;
+
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+  fetch(`http://localhost:3000/weather?address=${location}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          
+          messageTwo.textContent = data.error;
+        } else {
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
+        }
+      });
+    }
+  );
+
+
+});
